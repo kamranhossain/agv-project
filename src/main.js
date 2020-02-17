2d6c71fd1aac552407adb82e159d3c730a4273cb
kamranhossain <kamran.hossain227@gmail.com>
2020-02-18 01:51:48 +0600

    import all related files in src mainjs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
+// This is everything we need to work with Apollo 2.0.
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import VueApollo from 'vue-apollo';
+
+// Register the VueApollo plugin with Vue.
+Vue.use(VueApollo);
 Vue.config.productionTip = false
 
 new Vue({
